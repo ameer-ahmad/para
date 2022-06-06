f976f52c370d88f598bfe1f15ac0c2117e8e2093
ameer-ahmad <ameer5ahmad@gmail.com>
2022-06-06 13:16:57 -0400

    mobile responsiveness

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -4,20 +4,16 @@ import axios from 'axios';
 const Modal = ({meal, setModalIsOpen}) => {
 
     const [recipes, setRecipes] = useState(false)
-    const [products, setProducts] = useState(false)
-    const [menuItems, setMenuItems] = useState(false)
     const [info, setInfo] = useState({})
     const [ingredients, setIngredients] = useState([])
+    const [nutrients, setNutrients] = useState([])
+
 
     useEffect(() => {
         if (meal.mealType === 'recipes') {
             setRecipes(true)
             recipeInfo()
-        } else if (meal.mealType === 'products') {
-            setProducts(true)
-        } else if (meal.mealType === 'menuItems') {
-            setMenuItems(true)
-        }
+        } 
     }, [])
 
     
@@ -39,53 +35,54 @@ const Modal = ({meal, setModalIsOpen}) => {
           }).catch(function (error) {
               console.error(error);
           });
+
+          const options2 = {
+            method: 'GET',
+            url: `https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/${meal.id}/nutritionWidget.json`,
+            headers: {
+              'X-RapidAPI-Host': 'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com',
+              'X-RapidAPI-Key': '0e3f16cb99msh0c45ed889f095e2p1db952jsn93820868190d'
+            }
+        };
+
+        axios.request(options2).then(function (response) {
+            setNutrients({
+                calories: parseInt(response.data.calories.slice(0, -1)),
+                carbs: parseInt(response.data.carbs.slice(0, -1)),
+                fat: parseInt(response.data.fat.slice(0, -1)),
+                protein: parseInt(response.data.protein.slice(0, -1)),
+            })
+        }).catch(function (error) {
+            console.error(error);
+        });
     }
 
-    // const productNutrients = () => {
-    //     const options = {
-    //         method: 'GET',
-    //         url: `https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/food/products/${meal.id}`,
-    //         headers: {
-    //           'X-RapidAPI-Host': 'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com',
-    //           'X-RapidAPI-Key': '0e3f16cb99msh0c45ed889f095e2p1db952jsn93820868190d'
-    //         }
-    //       };
-          
-    //       axios.request(options).then(function (response) {
-    //           setNutrients({
-    //             calories: response.data.nutrition.calories,
-    //             carbs: parseInt(response.data.nutrition.carbs.slice(0, -1)),
-    //             fat: parseInt(response.data.nutrition.fat.slice(0, -1)),
-    //             protein: parseInt(response.data.nutrition.protein.slice(0, -1)),
-    //         })
-    //       }).catch(function (error) {
-    //           console.error(error);
-    //       });
-    // }
-
-    // const menuItemNutrients = () => {
-    //     const options = {
-    //         method: 'GET',
-    //         url: `https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/food/menuItems/${meal.id}`,
-    //         headers: {
-    //           'X-RapidAPI-Host': 'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com',
-    //           'X-RapidAPI-Key': '0e3f16cb99msh0c45ed889f095e2p1db952jsn93820868190d'
-    //         }
-    //       };
-    // }
-    
     return (
         <div className="modalContainer" onClick={() => {setModalIsOpen(false)}}>
             <div className="modal" onClick={(e) => e.stopPropagation()}>
                 {recipes ? (
-                    <>
-                        <img src={info.image} alt={info.title} />
-                        <h3>{info.title}</h3>
-                        <h4>{info.sourceName}</h4>
-                        <a href={info.sourceUrl} target="_blank" rel="noreferrer">Check out the recipe!</a>
-                        <p dangerouslySetInnerHTML={{__html: info.summary}}></p>
-                        <p>Recipe: </p>
-                        <button onClick={() => {setModalIsOpen(false)}}>Close</button>
+                    <>  
+                        <div className="modalAlignImageBtn">
+                            <div className="modalImageContainer">
+                                <img className="modalImage" src={info.image} alt={info.title} />
+                            </div>
+                            <a href={info.sourceUrl} target="_blank" rel="noreferrer"><div className="sourceLink">Source</div></a>
+                        </div>
+                        <div className="modalAlign">
+                            <div>
+                                <h3 className="modalTitle">{info.title}</h3>
+                                <h4 className="modalSource" >{info.sourceName}</h4>
+                            </div>
+                            <div className="mealNutrientsContainer">
+                                <span className="mealNutrients calories">{nutrients.calories} cal</span>
+                                <span className="mealNutrients carbs">{nutrients.carbs} carbs</span>
+                                <span className="mealNutrients fat">{nutrients.fat} fat</span>
+                                <span className="mealNutrients protein">{nutrients.protein} protein</span>
+                            </div>
+                        </div>
+                        <h5 className="modalDesc">Description</h5>
+                        <p className="modalSummary" dangerouslySetInnerHTML={{__html: info.summary}}></p>
+                        <button className="closeModal" onClick={() => {setModalIsOpen(false)}}>x</button>
                     </>
                 ) : null}
             </div>
